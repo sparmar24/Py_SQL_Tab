3315a1e9221d1d025f91d9cb079158b4d78613d2
Sonia Parmar <soniaparmar24@gmail.com>
2023-03-16 20:15:29 +0100

    Clean the raw data

---
+++ data_preprocessing.py
@@ -0,0 +1,70 @@
+import pandas as pd
+import numpy 
+
+df = pd.read_csv("Absenteeism_data.csv")
+raw = df.copy()
+
+#print(df_data)
+#print(df.head)
+# print(raw.columns)
+# print(df.info)
+
+df = df.drop(["ID"], axis=1)
+df_no_age = df.drop(["Age"], axis=1)
+# print(df_no_age.info)
+# print(df_no_age.columns)
+
+# print(df_no_age["Reason for Absence"])
+# print(df_no_age["Reason for Absence"].min())
+# print(df_no_age["Reason for Absence"].max())
+# print(df_no_age["Reason for Absence"].unique())
+# print(sorted(df_no_age["Reason for Absence"].unique()))
+
+
+reason_columns = pd.get_dummies(df_no_age["Reason for Absence"], drop_first=True)
+# print(reason_columns)
+age_dummies = pd.get_dummies(df["Age"])
+# print(age_dummies)
+# print(df.columns.values)
+df = df.drop(["Reason for Absence"], axis=1)
+
+reason_type_1 = reason_columns.loc[:, :14].max(axis=1) #-> operation horizontal axis
+reason_type_2 = reason_columns.loc[:, 15:17].max(axis=1) #-> operation horizontal axis
+reason_type_3 = reason_columns.loc[:, 18:21].max(axis=1) #-> operation horizontal axis
+reason_type_4 = reason_columns.loc[:, 22:].max(axis=1) #-> operation horizontal axis
+
+# print(df)
+df = pd.concat([df, reason_type_1, reason_type_2, reason_type_3, reason_type_4], axis=1)
+column_names = ['Date', 'Transportation Expense', 'Distance to Work',
+ 'Age', 'Daily Work Load Average', 'Body Mass Index', 'Education', 'Children',
+ 'Pet', 'Absenteeism Time in Hours', 'reason_1', 'reason_2', 'reason_3', 'reason_4' ]
+df.columns = column_names
+
+# print(df["Date"])
+column_names_reordered = ['reason_1', 'reason_2', 'reason_3', 'reason_4', 'Date', 'Transportation Expense', 'Distance to Work',
+ 'Age', 'Daily Work Load Average', 'Body Mass Index', 'Education', 'Children',
+ 'Pet', 'Absenteeism Time in Hours']
+df = df[column_names_reordered]
+# print(df)
+# print(df["Date"])
+df_concatenated = pd.concat([df_no_age, age_dummies], axis=1)
+# print(df_concatenated.columns.values)
+# print(df_no_age.columns.values)
+# print(age_dummies.columns.values)
+# df_concatenated = df_concatenated.drop(["Reason for Absence"], axis=1)
+column_names_rearranged = ['Reason for Absence', 'Date', 'Transportation Expense', 'Distance to Work',
+ 'Daily Work Load Average', 'Body Mass Index', 'Education', 'Children', 'Pet',
+ '27', '28', '29', '30', '31', '32', '33', '34', '36', '37', '38', '39', '40', '41', '43',
+                '46', '47', '48', '49', '50', '58', 'Absenteeism Time in Hours']
+# print(df_concatenated.columns.values)
+df_concatenated.columns = column_names_rearranged
+# print(df_concatenated)
+
+df_checkpoint = df_concatenated.copy()
+
+df_reason_mod = df.copy()
+df_reason_mod["Date"] = pd.to_datetime(df_reason_mod["Date"], format="%d/%m/%Y")
+# print(df_reason_mod.info())
+
+print(df_reason_mod["Date"][0])
+print(df_reason_mod["Date"][0].month)
